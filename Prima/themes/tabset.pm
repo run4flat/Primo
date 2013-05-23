951cab3f9d6dc0364d68ccd40022edd358d70f68
David Mertens <dcmertens.perl@gmail.com>
2013-05-23 18:09:00 -0500

    Removed unnecessary modules

--- Prima/themes/tabset.pm
+++
@@ -1,4 +0,0 @@
-# makes notebook tabset non-colored
-
-Prima::Themes::register( 'Prima::themes::tabset', 'tabset-gray', [ 'Prima::TabSet' => { colored => 0}]);
-Prima::Themes::register( 'Prima::themes::tabset', 'tabset-warp', [ 'Prima::TabSet' => { colored => 1}]);
